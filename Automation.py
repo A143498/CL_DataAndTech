7becc62593a795812953be5ddb424216072e188b
a143498 <martha_hause@progressive.com>
2023-04-28 17:03:14 -0400

    adding more to Self Service Proj Section

--- Automation.py
+++ Automation.py
@@ -29,7 +29,7 @@ Group By FORMAT(EmailSentDate, 'MM-yy')
 """
 
 LR_df = pd.read_sql(LR_Table, LR_connect)
-print(LR_df)
+# print(LR_df)
 
 LR_connect.close()
 
@@ -50,7 +50,7 @@ GROUP BY FORMAT(ClickDateTime, 'MM-yy');
 # Top 100 count(INFOID) as dis_INFOID,  distinct FORMAT(ClickDateTime, 'MM-yy') as date where ClickDateTime > '02-27-23'
 # execute the statement
 COI_df = pd.read_sql(COI_Table, COI_connect)
-print(COI_df)
+# print(COI_df)
 
 # close connection
 COI_connect.close()
@@ -59,6 +59,110 @@ COI_connect.close()
 # Bringing in Self-Service
 print("Self-Service Information")
 
+SelfService_connect = p.connect("DSN=MSS-P1-PCA-06")
 
+SelfService_Table = """
+Select A.eventmmyy,
+a.pagename,
+--a.pagetype,
+a.applicantgroups,
+A.PRODUCT_GROUP,
+SUM(a.pagenamecount) as pagehits FROM
+(SELECT
+	--CONVERT(date,DateAdd(hour,-5,[eventTimestamp])) as eventdate,
+	[EventDate] as eventmmyy,
+	CASE WHEN [pagetype] = 'BASE_PAGE' THEN concat([pagename],'@') ELSE [pagename] end AS [pagename],
+	--[pagetype],
+
+	CASE WHEN [userdata userroletxt] LIKE '%Agent%' AND [geocarrier] <>  'Progressive Casualty Insurance Companies' AND [userdata AgentCode] <> 'null' then 'Agent'
+               WHEN [userdata userroletxt] = 'null' AND [geocarrier] <>  'Progressive Casualty Insurance Companies' AND [userdata AgentCode] = 'null' AND [pagename] LIKE '%foragentsonly%' then 'Agent'
+               WHEN [userdata UserRoleTxt] <> 'Agent' and  [userdata PartyLogonId] IN ('holder', 'proxy') AND [geocarrier] <> 'Progressive Casualty Insurance Companies' then 'Customer'
+               WHEN [userdata userroletxt] = 'null' AND [geocarrier] <>  'Progressive Casualty Insurance Companies' AND [userdata PartyLogonId] = 'null' AND [pagename] LIKE '%progressivecommercial%' then 'Customer'
+               WHEN  [userdata UserRoleTxt] = 'null' AND [geocarrier] <>  'Progressive Casualty Insurance Companies' then 'Unknown'
+               WHEN [geocarrier] = 'Progressive Casualty Insurance Companies' then 'Internal'
+              ELSE  'Internal' END AS applicantgroups,
+
+	--CASE WHEN [pagename] IN ('businessownerinfo', 'enterrequestdetails', 'reissue', 'cancel', 'reinstate', 'nonrenew') THEN 'BEGINNING TRANS'
+	--WHEN [pagename] IN ('quotesoldconfirm','endorseconfirm', 'ReinstateConfirm', 'NonRenewRBS', 'makepayment') THEN 'ENDPOINTS'
+	--ELSE 'NA' END AS TRANS_TYPES,
+
+	CASE WHEN  [userdata ProductCode] = 'CV' then 'Quoting'
+	WHEN [userdata ProductCode] = 'CA' then 'Servicing'
+	ELSE 'NA' END AS PRODUCT_GROUP,
+
+	count([pagename]) as pagenamecount
+	FROM [CLExperience].[dbo].[APPDCSV]
+	WHERE
+  --CONVERT(datetime,DateAdd(hour,-5,[eventTimestamp])) >  '2022-11-10 00:00:00.000'
+  -- Oct 3rd-Oct 29th VS Nov 3rd-Dec 1st
+	Eventdate between '2022-12-31' and '2023-02-03'
+	--Eventdate between '2022-11-03' and '2022-12-01'
+	AND [pagename] IN ('EndorseRBS', 'VehicleGaragingInformation', 'VehicleGaragingInformation-Reload')
+	AND [userdata ProductCode] = 'CA'
+	Group By
+	--CONVERT(date,DateAdd(hour,-5,[eventTimestamp])),
+	[Eventdate],
+	[pagename],
+	[pagetype],
+	[userdata userroletxt], [geocarrier], [userdata AgentCode], [pagename], [userdata PartyLogonId], [userdata ProductCode]) A
+
+Group by A.eventmmyy,
+a.pagename,
+--a.pagetype,
+a.applicantgroups,
+
+A.PRODUCT_GROUP
+Order By A.eventmmyy
+"""
+
+
+SelfService_df = pd.read_sql(SelfService_Table, SelfService_connect)
+# print(SelfService_df)
+
+# join dimdate table in FSScoreCard to get Prog Month for Tableau
+
+ProgMonths_connect = p.connect("DSN=MSS-P1-PCA-06")
+
+ProgMonths_Table = """
+SELECT DT_VAL, ACCT_CCYYMM
+FROM [FSScoreCard].[CLCQ].[DimDate]
+  where ACCT_CCYY >= 2022
+"""
+
+ProgMonths_df = pd.read_sql(ProgMonths_Table, ProgMonths_connect)
+# print(ProgMonths_df)
+
+# join the two tables above to get the Prog Months Correctly
+print("")
+print("Prog Self Service With Prog Month")
+ProgSelfService_df = pd.merge(
+    ProgMonths_df, SelfService_df, how="inner", left_on="DT_VAL", right_on="eventmmyy"
+)
+
+print(
+    ProgSelfService_df.groupby(
+        [
+            "ACCT_CCYYMM",
+            "pagename",
+            "applicantgroups",
+            "PRODUCT_GROUP",
+        ]
+    )["pagehits"].sum()
+)
+
+# Prog_Self = """
+# SELECT ACCT_CCYYMM, PAGENAME, APPLICANTGROUPS, PRODUCT_GROUP, SUM(pagehits) as PageHiters
+# FROM ProgSelfService_df
+# Group by ACCT_CCYYMM, PAGENAME, APPLICANTGROUPS, PRODUCT_GROUP
+# """
+
+# print(ProgSelfService_df)
+print("")
+# print(Prog_Self)
+# close connection
+SelfService_connect.close()
+ProgMonths_connect.close()
+
+print("")
 # Bringing in Session Time Reduced
 print("Session Time Reduced Information")
